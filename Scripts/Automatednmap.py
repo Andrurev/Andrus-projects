3f35ec728045649856a9112797771e95ad182d9a
Andrurev <120023220+Andrurev@users.noreply.github.com>
2022-12-13 07:45:21 -0800

    Update Automatednmap.py

--- Scripts/Automatednmap.py
+++ Scripts/Automatednmap.py
@@ -1,9 +1,64 @@
-ere is a sample automation script for cybersecurity that can be used to scan a network for vulnerabilities:
+here is a sample automation script for cybersecurity that can be used to scan a network for vulnerabilities and send email notifs about findings:
 
 
 # Import the necessary modules
 import os
 import nmap
+
+# Import the argparse module
+import argparse
+
+# Import the necessary modules
+import nmap
+import smtplib
+
+# Import the argparse module
+import argparse
+
+# Set up the argparse argument parser
+parser = argparse.ArgumentParser()
+
+# Add an argument for the type of scan to perform
+parser.add_argument("--scan", choices=["SYN", "UDP"], help="Type of scan to perform")
+
+# Parse the command line arguments
+args = parser.parse_args()
+
+# Set the target IP range for the scan
+ip_range = "192.168.1.1-254"
+
+# Set the email server and login credentials
+email_server = "smtp.example.com"
+email_user = "username"
+email_pass = "password"
+
+# Initialize the nmap scanner
+scanner = nmap.PortScanner()
+
+# Scan the network for open ports and vulnerabilities
+scan_results = scanner.scan(ip_range, arguments="-sV -sC")
+
+# Iterate over the hosts in the scan results
+for host in scan_results["scan"]:
+  ip = host[0]
+  open_ports = host[1]["tcp"].keys()
+
+  # Print the IP address and open ports for the current host
+  print(f"Host: {ip}")
+  print(f"Open ports: {open_ports}")
+
+  # Check if the host has any vulnerabilities
+  if "vulns" in host[1]:
+    # If the host has
+
+# Set up the argparse argument parser
+parser = argparse.ArgumentParser()
+
+# Add an argument for the type of scan to perform
+parser.add_argument("--scan", choices=["SYN", "UDP"], help="Type of scan to perform")
+
+# Parse the command line arguments
+args = parser.parse_args()
 
 # Set the target IP range for the scan
 ip_range = "192.168.1.1-254"
